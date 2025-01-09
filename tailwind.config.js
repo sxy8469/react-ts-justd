56b2960fb1a58f8e34ad54b6b9e765daa2e60081
sxy <sun.xiangyu@cn.22miles.com>
2025-01-09 15:53:07 +0800

    first

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+// import { fontFamily } from "tailwindcss/defaultTheme";
+
+/** @type {import("tailwindcss").Config} */
+export default {
+    // content: [
+    //     "./index.html",
+    //     "./src/**/*.{js,ts,jsx,tsx}",
+    // ],
+    // darkMode: 'selector',
+    // theme: {
+    //     extend: {
+    //         fontFamily: {
+    //             sans: ["InterVariable", ...fontFamily.sans],
+    //             mono: ["InterVariable", ...fontFamily.mono],
+    //         },
+    //         // spacing: {
+    //         //     "128": "32rem",
+    //         //     "144": "36rem",
+    //         // },
+    //         // borderRadius: {
+    //         //     "4xl": "2rem",
+    //         // },
+    //     },
+    // },
+    // plugins: [
+    //     require("tailwindcss-animate"),
+    // ],
+};
---
+++ vite.config.ts
@@ -0,0 +1,60 @@
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite';
+import tsconfigPaths from "vite-tsconfig-paths";
+import * as path from "node:path";
+
+// https://vite.dev/config/
+export default defineConfig(({mode}) => {
+    return {
+        define: {
+            "process.env.NODE_ENV": JSON.stringify(mode),
+        },
+        resolve: {
+            alias: {
+                "@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs",
+            },
+        },
+        plugins: [
+            tailwindcss(),
+            react(),
+            tsconfigPaths()
+        ],
+        build: {
+            sourcemap: false,
+            reportCompressedSize: false,
+            rollupOptions: {
+                output: {
+                    // manualChunks: {
+                    //     vendor: ['react', 'react-router-dom', 'react-dom'],
+                    //     ...renderChunks(dependencies),
+                    // },
+                    manualChunks: (id:any) => {
+                        if (id.includes("node_modules")) {
+                            const packageName = get_package_name(id);
+                            if(react_package_names.includes(packageName)) {
+                                return "react-vendor";
+                            }else{
+                                return "vendor";
+                            }
+                        }else{
+                            // const package_path = path.normalize(id);
+                            // if(package_path.includes(src_path)){
+                            //     return "home";
+                            // }
+                        }
+                    },
+                },
+            },
+        },
+    };
+});
+
+const react_package_names = ["react", "react-dom", "react-router-dom"];
+const src_path = path.normalize(path.resolve(__dirname, "./src"));
+function get_package_name(id: string) {
+    let segments = id.split('node_modules/');
+    segments = segments[segments.length - 1].split('/');
+    // const segments = id.split('node_modules/')[1].split('/');
+    return segments[0].startsWith('@') ? `${segments[0]}/${segments[1]}` : segments[0];
+}
